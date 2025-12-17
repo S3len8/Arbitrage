e7b25f34d0635a87936a839da6d3b10c7731d493
Denys <glushychden.10@gmail.com>
2025-12-17 20:14:30 +0200

    Додав функцію для перегляду ціни Біткоіну

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from binance import Client
+from binance.client import Client
 
+client = Client()  # для публічних даних API ключі не потрібні
 
-client = Client(api_key='', api_secret='')
+# client = Client(api_key='', api_secret='')
 
-prices = client.get_all_tickers()
+# prices = client.get_all_tickers()
+
+def get_price(symbol: str):
+    data = client.get_symbol_ticker(symbol=symbol)
+    return float(data['price'])
+
+print(get_price("BTCUSDT"))
