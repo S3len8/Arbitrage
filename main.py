ef1799cec53126d5effba33071c369c8e5c4dd15
Denys <glushychden.10@gmail.com>
2025-12-17 20:56:02 +0200

    Створено функцію для того, щоб отримувати ціну монети та монету з тікером USDT, але варіант USDTETH не підходить

--- main.py
+++ main.py
@@ -4,10 +4,30 @@ client = Client()  # для публічних даних API ключі не п
 
 # client = Client(api_key='', api_secret='')
 
-# prices = client.get_all_tickers()
+tickers = client.get_all_tickers()
+exchange_info = client.get_exchange_info()
+
+# словник тільки USDT пар
+USDT_PRICES = {
+    p['symbol']: float(p['price'])
+    for p in tickers
+    if p['symbol'].endswith('USDT')
+}
+
+# value_usdt = USDT_PRICES.get('key')
 
 def get_price(symbol: str):
     data = client.get_symbol_ticker(symbol=symbol)
     return float(data['price'])
 
 print(get_price("BTCUSDT"))
+
+print(USDT_PRICES)
+
+# print(value_usdt)
+
+# print(tickers)
+
+
+
+
