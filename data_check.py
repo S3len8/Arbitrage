808ddeb90b9695e991c32362c262d5d6f235af6e
Denys <glushychden.10@gmail.com>
2025-12-19 23:06:28 +0200

    Розділення файлів. В головному треба написати формули, в дата чек отримання цін, комісії і передача їх в головний файл. Додав записання цін в словник

--- data_check.py
+++ data_check.py
@@ -1,33 +1,49 @@
 from binance.client import Client
 
-client = Client()  # для публічних даних API ключі не потрібні
+binance_client = Client()  # для публічних даних API ключі не потрібні
+#
+# # client = Client(api_key='', api_secret='')
+#
+# tickers = client.get_all_tickers()
+# exchange_info = client.get_exchange_info()
+#
+# # словник тільки USDT пар
+# USDT_PRICES = {
+#     p['symbol']: float(p['price'])
+#     for p in tickers
+#     if p['symbol'].endswith('USDT')
+# }
+#
+# # value_usdt = USDT_PRICES.get('key')
+#
 
-# client = Client(api_key='', api_secret='')
-
-tickers = client.get_all_tickers()
-exchange_info = client.get_exchange_info()
-
-# словник тільки USDT пар
-USDT_PRICES = {
-    p['symbol']: float(p['price'])
-    for p in tickers
-    if p['symbol'].endswith('USDT')
-}
-
-# value_usdt = USDT_PRICES.get('key')
 
 def get_price(symbol: str):
-    data = client.get_symbol_ticker(symbol=symbol)
+    data = binance_client.get_symbol_ticker(symbol=symbol)
     return float(data['price'])
 
-print(get_price("BTCUSDT"))
 
-print(USDT_PRICES)
+prices = {
+    'Binance': get_price,
+    'Bybit': '',
+}
 
-# print(value_usdt)
+# Fees for binance, bybit and others cryptocurrency exchange
+# fees = {
+#     'Binance': '0.01',
+#     'Bybit': '0.01',
+# }
 
-# print(tickers)
-
-
-
-
+print(prices['Binance']('BTCUSDT'))
+#
+# print(get_price("BTCUSDT"))
+#
+# print(USDT_PRICES)
+#
+# # print(value_usdt)
+#
+# # print(tickers)
+#
+#
+#
+#
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import data_check
+from data_check import get_price
+
+
+def best_exchange():
+    return get_price("BTCUSDT")
+
+
+print(best_exchange())
+
+
+# profit = spread - buy_fees - sell_fees - withdrawal_fees
 
 
 
