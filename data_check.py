7cb2c8bd5bfc7464283644fc5133a1825abf136f
Denys <glushychden.10@gmail.com>
2025-12-19 15:55:04 +0200

    Оновлено основний файл, де будуть функції. Додано дата чек, де буде збирання інформації

---
+++ data_check.py
@@ -0,0 +1,33 @@
+from binance.client import Client
+
+client = Client()  # для публічних даних API ключі не потрібні
+
+# client = Client(api_key='', api_secret='')
+
+tickers = client.get_all_tickers()
+exchange_info = client.get_exchange_info()
+
+# словник тільки USDT пар
+USDT_PRICES = {
+    p['symbol']: float(p['price'])
+    for p in tickers
+    if p['symbol'].endswith('USDT')
+}
+
+# value_usdt = USDT_PRICES.get('key')
+
+def get_price(symbol: str):
+    data = client.get_symbol_ticker(symbol=symbol)
+    return float(data['price'])
+
+print(get_price("BTCUSDT"))
+
+print(USDT_PRICES)
+
+# print(value_usdt)
+
+# print(tickers)
+
+
+
+
