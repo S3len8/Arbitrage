2383b1182388cb52d58f2fea2fce8b10f737b715
Denys <glushychden.10@gmail.com>
2025-12-20 13:43:12 +0200

    Створено дві функції для обрахунку різниці бідів на біржах, та асків на біржах. Кортеж перевів в список, щоб отримати інформацію про ціну і робити з цим маніпуляції у вигляді функцій

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 from data_check import get_price_binance, get_price_bybit
 
 
-# def best_exchange():
-#     return get_price_binance("BTCUSDT")
-
-
 prices = {
     'Binance': get_price_binance,
     'Bybit': get_price_bybit,
 }
 
-# print(best_exchange())
+data_binance = list(prices['Binance']('BTCUSDT'))
+data_bybit = list(prices['Bybit']('BTCUSDT'))
 
+price_binance_bid, price_binance_ask = float(data_binance[2]), float(data_binance[1])
+price_bybit_bid, price_bybit_ask = float(data_bybit[1]), float(data_bybit[2])
+
+
+def calc_bid(firstCurrency, secondCurrency):
+    if firstCurrency > secondCurrency:
+        result = firstCurrency - secondCurrency
+        return result
+    if secondCurrency > firstCurrency:
+        result = secondCurrency - firstCurrency
+        return result
+
+
+def calc_ask(firstCurrency, secondCurrency):
+    if firstCurrency > secondCurrency:
+        result = firstCurrency - secondCurrency
+        return result
+    if secondCurrency > firstCurrency:
+        result = secondCurrency - firstCurrency
+        return result
+
+
+# print(calc(prices['Binance']('BTCUSDT'), prices['Bybit']('BTCUSDT')))
+# result = float(" ".join(map(str, prices['Binance']('BTCUSDT')[1][2])))
+# prices_1 = float(data_binance[1]) - float(data_bybit[2])
+# print(prices_1)
+print(calc_bid(price_binance_bid, price_bybit_bid))
+print(calc_ask(price_binance_ask, price_bybit_ask))
 print(prices['Binance']('BTCUSDT'), prices['Bybit']('BTCUSDT'))
 
 
-# profit = spread - buy_fees - sell_fees - withdrawal_fees
 
 
 
