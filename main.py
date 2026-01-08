1bb05271f920a6e5e8e5c6a8e4afea276347d809
Denys <glushychden.10@gmail.com>
2026-01-08 02:38:34 +0200

    Cleared files from useless strings.

--- main.py
+++ main.py
@@ -42,12 +42,7 @@ def calc_spread(binance_bid, bybit_bid, binance_ask, bybit_ask):
         return print(f'Spread is {spread:.9%}')
 
 
-# print(calc(prices['Binance']('BTCUSDT'), prices['Bybit']('BTCUSDT')))
-# result = float(" ".join(map(str, prices['Binance']('BTCUSDT')[1][2])))
-# prices_1 = float(data_binance[1]) - float(data_bybit[2])
-# print(prices_1)
-# print(calc_bid(price_binance_bid, price_bybit_bid))
-# print(calc_ask(price_binance_ask, price_bybit_ask))
+
 calc_spread(price_binance_bid, price_bybit_bid, price_binance_ask, price_bybit_ask)
 print(prices['Binance']('BTCUSDT'), prices['Bybit']('BTCUSDT'))
 
