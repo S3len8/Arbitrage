322b1b4b8ad35077385ad585cb9ee28588535b4a
Denys <glushychden.10@gmail.com>
2026-01-08 23:31:11 +0200

    Updated functions for get informations about BTC, ETH, SOL. Best method to get bid/ask prices.

--- data_check.py
+++ data_check.py
@@ -4,13 +4,13 @@ from binance.client import Client
 binance_client = Client()  # для публічних даних API ключі не потрібні
 
 
-def get_price_binance():
+def get_price_binance(symbols: list[str]) -> dict:
     data = binance_client.get_orderbook_tickers()
     result = {}
 
     for item in data:
         symbol = item["symbol"]
-        if symbol.endswith("USDT"):
+        if symbol in symbols:
             result[symbol] = {
                 "bid": float(item["bidPrice"]),
                 "ask": float(item["askPrice"])
@@ -19,20 +19,19 @@ def get_price_binance():
     return result
 
 
-def get_price_bybit(symbol: str):
+def get_price_bybit(symbols: list[str]) -> dict:
     url = "https://api.bybit.com/v5/market/tickers"
     params = {"category": "spot"}
+
     r = requests.get(url, params=params, timeout=5).json()
+    result = {}
 
     for item in r["result"]["list"]:
-        if item["symbol"] == symbol:
-            return (
-                item["symbol"],
-                float(item["bid1Price"]),
-                float(item["ask1Price"])
-            )
+        symbol = item["symbol"]
+        if symbol in symbols:
+            result[symbol] = {
+                "bid": float(item["bid1Price"]),
+                "ask": float(item["ask1Price"])
+            }
 
-
-print(get_price_binance())
-
-
+    return result
--- main.py
+++ main.py
@@ -2,58 +2,69 @@ import time
 
 from data_check import get_price_binance, get_price_bybit
 
+SYMBOLS = ["BTCUSDT", "ETHUSDT", "SOLUSDT"]
 
-prices = {
-    'Binance': get_price_binance,
-    'Bybit': get_price_bybit,
-}
-
-data_binance = list(prices['Binance']('BTCUSDT'))
-data_bybit = list(prices['Bybit']('BTCUSDT'))
-
-price_binance_bid, price_binance_ask = float(data_binance[2]), float(data_binance[1])
-price_bybit_bid, price_bybit_ask = float(data_bybit[1]), float(data_bybit[2])
-
-
-def calc_bid(firstCurrency, secondCurrency):
-    if firstCurrency > secondCurrency:
-        result = firstCurrency - secondCurrency
-        return result
-    if secondCurrency > firstCurrency:
-        result = secondCurrency - firstCurrency
-        return result
+# prices = {
+#     'Binance': get_price_binance,
+#     'Bybit': get_price_bybit,
+# }
+#
+# data_binance = list(prices['Binance']('BTCUSDT'))
+# data_bybit = list(prices['Bybit']('BTCUSDT'))
+#
+# price_binance_bid, price_binance_ask = float(data_binance[2]), float(data_binance[1])
+# price_bybit_bid, price_bybit_ask = float(data_bybit[1]), float(data_bybit[2])
 
 
-def calc_ask(firstCurrency, secondCurrency):
-    if firstCurrency > secondCurrency:
-        result = firstCurrency - secondCurrency
-        return result
-    if secondCurrency > firstCurrency:
-        result = secondCurrency - firstCurrency
-        return result
+# def calc_bid(firstCurrency, secondCurrency):
+#     if firstCurrency > secondCurrency:
+#         result = firstCurrency - secondCurrency
+#         return result
+#     if secondCurrency > firstCurrency:
+#         result = secondCurrency - firstCurrency
+#         return result
+#
+#
+# def calc_ask(firstCurrency, secondCurrency):
+#     if firstCurrency > secondCurrency:
+#         result = firstCurrency - secondCurrency
+#         return result
+#     if secondCurrency > firstCurrency:
+#         result = secondCurrency - firstCurrency
+#         return result
 
 
-def calc_spread(binance_bid, bybit_bid, binance_ask, bybit_ask):
-    if binance_bid > bybit_bid:
-        print('buy BTC Bybit')
-        spread = ((binance_ask - bybit_bid) / binance_ask)
-        return print(f'Spread is {spread:.9%}')
-    if bybit_bid > binance_bid:
-        print('buy BTC Binance')
-        spread = ((bybit_ask - binance_bid) / bybit_ask)
-        return print(f'Spread is {spread:.9%}')
+def calc_spread(symbol, binance, bybit):
+    if binance["bid"] > bybit["bid"]:
+        spread = (binance["ask"] - bybit["bid"]) / binance["ask"]
+        side = "BUY Bybit → SELL Binance"
+    else:
+        spread = (bybit["ask"] - binance["bid"]) / bybit["ask"]
+        side = "BUY Binance → SELL Bybit"
+
+    return side, spread * 100
 
 
 while True:
-    binance_symbol, binance_ask, binance_bid = get_price_binance('BTCUSDT')
-    bybit_symbol, bybit_bid, bybit_ask = get_price_bybit('BTCUSDT')
+    binance_data = get_price_binance(SYMBOLS)
+    bybit_data = get_price_bybit(SYMBOLS)
 
-    calc_spread(binance_bid, bybit_bid, binance_ask, bybit_ask)
+    for symbol in SYMBOLS:
+        if symbol not in binance_data or symbol not in bybit_data:
+            continue
 
-    print(
-        f"Binance bid/ask: {binance_bid} / {binance_ask}",
-        f"Bybit bid/ask: {bybit_bid} / {bybit_ask}"
-    )
+        side, spread = calc_spread(
+            symbol,
+            binance_data[symbol],
+            bybit_data[symbol]
+        )
+
+        print(
+            f"{symbol} | {side} | Spread: {spread:.4f}% | "
+            f"Binance {binance_data[symbol]['bid']}/{binance_data[symbol]['ask']} | "
+            f"Bybit {bybit_data[symbol]['bid']}/{bybit_data[symbol]['ask']}"
+            f"\n ==============================================================="
+        )
 
     time.sleep(7)
 
