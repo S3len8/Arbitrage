454881e3ae27ac21e0eeaa1fddca9afb67d06d77
Denys <glushychden.10@gmail.com>
2026-01-08 02:35:28 +0200

    Added function for calculated spread. Function have two if`s. First if can give information about buy btc Bybit. Second if can give information about buy btc Binance. After all information about difference in currency`s we can write to spread and calculate % and give to users.

--- main.py
+++ main.py
@@ -31,12 +31,24 @@ def calc_ask(firstCurrency, secondCurrency):
         return result
 
 
+def calc_spread(binance_bid, bybit_bid, binance_ask, bybit_ask):
+    if binance_bid > bybit_bid:
+        print('buy BTC Bybit')
+        spread = ((binance_ask - bybit_bid) / binance_ask)
+        return print(f'Spread is {spread:.9%}')
+    if bybit_bid > binance_bid:
+        print('buy BTC Binance')
+        spread = ((bybit_ask - binance_bid) / bybit_ask)
+        return print(f'Spread is {spread:.9%}')
+
+
 # print(calc(prices['Binance']('BTCUSDT'), prices['Bybit']('BTCUSDT')))
 # result = float(" ".join(map(str, prices['Binance']('BTCUSDT')[1][2])))
 # prices_1 = float(data_binance[1]) - float(data_bybit[2])
 # print(prices_1)
-print(calc_bid(price_binance_bid, price_bybit_bid))
-print(calc_ask(price_binance_ask, price_bybit_ask))
+# print(calc_bid(price_binance_bid, price_bybit_bid))
+# print(calc_ask(price_binance_ask, price_bybit_ask))
+calc_spread(price_binance_bid, price_bybit_bid, price_binance_ask, price_bybit_ask)
 print(prices['Binance']('BTCUSDT'), prices['Bybit']('BTCUSDT'))
 
 
